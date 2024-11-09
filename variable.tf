97533a8334e474880ed94c9047dc7cfa67ebc8f2
bolaji-atmost <bolaji.adams@atmost.io>
2024-11-09 14:08:58 +0100

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "images" {
+  type    = string
+  default = "ubuntu:focal"
+}
+
+variable "ubuntu" {
+  type = map(any)
+  default = {
+    "focal"   = "ubuntu:22.04"
+    "bionic"  = "images:ubuntu/bionic/cloud"
+    "xenial"  = "images:ubuntu/xenial/cloud"
+    "kinetic" = "images:ubuntu/kinetic/cloud"
+    "impish"  = "images:ubuntu/impish/cloud"
+    "jammy"   = "images:ubuntu/jammy/cloud"
+  }
+}
+
+variable "node_name" {
+  type = map(string)
+  default = {
+    chatwoot = "kg_chatwoot_node"
+  }
+}
